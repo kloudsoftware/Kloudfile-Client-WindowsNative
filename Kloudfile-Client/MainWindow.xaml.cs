340c495996f4d78bef0f1a207a9e62fe4ace9093
Lars <lars@kloud.software>
2018-07-15 03:53:16 +0200

    added ScreenShotForm and rectangle drawing

--- Kloudfile-Client/MainWindow.xaml.cs
+++ Kloudfile-Client/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Forms;
 
 namespace Kloudfile_Client
 {
@@ -23,6 +26,106 @@ namespace Kloudfile_Client
         public MainWindow()
         {
             InitializeComponent();
+
+            var iconStream = System.Windows.Application.GetResourceStream(new Uri("pack://application:,,,/Images/Launch.ico"))?.Stream;
+            var ni = new System.Windows.Forms.NotifyIcon { Icon = new System.Drawing.Icon(iconStream), Visible = true };
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+
+
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+    [In] IntPtr hWnd,
+    [In] int id,
+    [In] uint fsModifiers,
+    [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9000;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
+
+        private void RegisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            const uint VK_F10 = 0x79;
+            const uint MOD_CTRL = 0x0002;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F10))
+            {
+                // handle error
+            }
+        }
+
+        private void UnregisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed()
+        {
+            Form f = new ScreenShotForm();
+            f.BackColor = System.Drawing.Color.WhiteSmoke;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.Bounds = Screen.AllScreens.Select(screen => screen.Bounds).Aggregate(System.Drawing.Rectangle.Union);
+            f.TopMost = true;
+            f.Opacity = 0.05;
+
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.Run(f);
+        }
+
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
         }
     }
 }
---
+++ Kloudfile-Client/ScreenShotForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kloudfile_Client
+{
+    public partial class ScreenShotForm : Form
+    {
+        private Point MouseDownLocation;
+
+        public ScreenShotForm()
+        {
+            this.DoubleBuffered = true;
+        }
+
+        Rectangle rec = new Rectangle(0, 0, 0, 0);
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Black, rec);
+        }
+
+        Tuple<int, int> startPos = null;
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPos = new Tuple<int, int>(e.X, e.X);
+                rec = new Rectangle(e.X, e.Y, 0, 0);
+                Invalidate();
+
+            }
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                rec.Width = e.X + rec.X;
+                rec.Height = e.Y + rec.Y;
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
